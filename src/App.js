adeb5785312b06a476a8e0a49b1d9d4aa442a24e
Amir Ali <Amir.dev@pk.see.biz>
2021-04-23 00:16:26 -0700

    working on creating person and first component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import react, { Component } from 'react'
 import './App.css';
+import First from './Components/First'
+import Person from './Components/person'
+import styled from 'styled-components'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    state = {
+        persons: [
+            { name: 'abc', age: 28 },
+            { name: 'xzy', age: 29 },
+            { name: 'Stephanie', age: 26 }
+
+        ]
+    }
+    namehandler = (newname) => {
+        this.setState({
+
+            persons: [
+                { name: 'fasdfs', age: 28 },
+                { name: 'XYZ', age: 19 },
+                { name: 'Step', age: 27 }
+            ]
+        })
+    }
+    render() {
+        // console.log('render component')
+        return (
+            <div className="App">
+        	 <First />
+        	 <button onClick={this.namehandler} className="btn btn-success">Change name</button>
+        	 <Person  name={this.state.persons[0].name}
+          age={this.state.persons[0].age}></Person>
+        	 <Person  name={this.state.persons[1].name}
+          age={this.state.persons[1].age}></Person>
+        	 <Person  name={this.state.persons[2].name}
+          age={this.state.persons[2].age}></Person>
+
+        	</div>
+        );
+    }
 }
 
 export default App;
